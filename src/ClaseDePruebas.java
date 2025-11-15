7bf0addad622cc023fa3b8e095d51d2dd3fd67e0
Oswaldo <santoswaldos@gmail.com>
2025-11-14 18:06:18 -0600

    Esta clase tiene la funcion de poder desarrollar ejercicios del profesor para ver el funcionamiento de los códigos

--- src/ClaseDePruebas.java
+++ src/ClaseDePruebas.java
@@ -1,7 +1,38 @@
 import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-public class ClaseDePruebas {
-    public static void main(String[] args) {
+    public class ClaseDePruebas {
+        public static void main(String[] args) {
 
+            int opcionIndice = 0;
+
+
+// los Map (o mapas) los veremos mas adelante en el curso en profundidad
+// pero son como un arreglo asociativo, asocia un nombre (o indice) a un valor
+// también se les conoce como diccionarios para almacenar datos en base a un nombre.
+
+            Map<String, Integer> opciones = new HashMap<>();
+            opciones.put("Actualizar", 1);
+            opciones.put("Eliminar", 2);
+            opciones.put("Agregar", 3);
+            opciones.put("Listar", 4);
+            opciones.put("Salir", 5);
+
+            Object[] opArreglo = opciones.keySet().toArray();
+
+            Object opcion = JOptionPane.showInputDialog(null,
+                    "Seleccione un Opción",
+                    "Mantenedor de Productos",
+                    JOptionPane.INFORMATION_MESSAGE, null, opArreglo, opArreglo[0]);
+
+            if (opcion == null) {
+                JOptionPane.showMessageDialog(null, "Debe seleccionar una operación");
+            } else {
+                opcionIndice = opciones.get(opcion.toString());
+
+                // aca un if o switch para las distintas opciones.
+            }
+        }
     }
-}
